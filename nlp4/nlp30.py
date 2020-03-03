51b03166d92e7428c6dc8978be0143dee110ed87
Shinya Todaka <tdk0105.strawberry@gmail.com>
2020-03-03 22:25:50 +0900

    進めた

--- nlp4/nlp30.py
+++ nlp4/nlp30.py
@@ -8,25 +8,49 @@ def make_neko_txt_mecab():
         with open("neko.txt.mecab","w") as f:
             f.write(m.parse(lines))
 
-def make_morpheme() -> []:
+def make_morpheme():
     with open("neko.txt.mecab") as f:
         morpheme_list = []
         for line in f:
+                if len(line.split()) < 2:
+                    return
                 surface = line.split()[0]
                 other_list = line.split()[1].split(",")
                 if (len(other_list) > 8):
                     dictionary = {}
                     dictionary["surface"] = surface
-                    dictionary["base"] = other_list[7]
+                    dictionary["base"] = other_list[6]
                     dictionary["pos"] = other_list[0]
                     dictionary["pos1"] = other_list[1]
                     morpheme_list.append(dictionary)
                     if surface == "。":
                         yield morpheme_list
                         morpheme_list = []
-n = 0
-for list in make_morpheme():
-    n += 1
-    pprint.pprint(list)
-    if n > 30:
-        break
+
+#qiitaでみたやり方
+def parse_line(line):
+    (surface,attr) = line.split('\t')
+    arr = attr.split(',')
+    return {
+        "surface":surface,
+        "base":arr[6],
+        "pos":arr[0],
+        "pos1":arr[1]
+    }
+
+def read_mecab_file(filename) -> [[{}]]:
+    sentence = []
+    with open(filename,mode='rt',encoding='utf-8') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            if line == "EOS":
+                yield sentence
+                sentence = []
+            else:
+                sentence.append(parse_line(line))
+# n = 0
+# for list in read_mecab_file("neko.txt.mecab_unix"):
+#     n += 1
+#     pprint.pprint(list)
+#     if n > 30:
+#         break
